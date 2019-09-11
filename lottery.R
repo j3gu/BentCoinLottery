7676255180537fc388db98a091d9e4f547bc19cb
JING GU <jing121812@gmail.com>
2019-09-11 15:01:40 -0400

    Better code by JG
    Made the codes more readable

--- lottery.R
+++ lottery.R
@@ -10,16 +10,18 @@
 library(stats)
 
 # Small edit! from class 1!
-
-arr <- array(0,10)
-for (i in 1:10){
-  arr[i] <- if (runif(1) < 0.1) 1 else 0
+num_of_flips <- 10
+num_of_coins_per_flip<-1
+num_of_tickets<-100
+flip_coins<-function(flips, coins){
+  arr_of_flips <- array(0,flips)
+    for (i in 1:flips){
+        arr_of_flips[i] <- if (runif(coins) < 0.1) 1 else 0
+    }
+  return(arr_of_flips)
 }
 
-for (j in 1:100){
-  arr <- array(0,10)
-  for (i in 1:10){
-    arr[i] <- if (runif(1) < 0.1) 1 else 0
-  }
-  print(arr)
+for (j in 1:num_of_tickets){
+  coin_flips<- flip_coins(num_of_flips, num_of_coins_per_flip)
+  print(coin_flips)
 }
